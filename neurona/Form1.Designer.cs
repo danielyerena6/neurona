eb5fae993d4a11e2f8c4184210617676db53f008
blaster <blasterdaxx2208@gmail.com>
2020-11-13 01:47:34 -0600

    neurona funcional

---
+++ neurona/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace neurona
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(117, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "No. entradas";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 223);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 215);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(204, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Ingresar Entradas";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(265, 44);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(177, 150);
+            this.dataGridView2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(262, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Pesos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(470, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Umbral";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(473, 44);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(473, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Neurona";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(473, 141);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "limpiar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ neurona/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace neurona
+{
+    public partial class Form1 : Form
+    {
+        int no_entradas;
+        int rows;
+        public Form1()
+        {
+            InitializeComponent();
+            DataGridViewTextBoxColumn pesos = new DataGridViewTextBoxColumn();
+            pesos.HeaderText = "Pesos"  ;
+            pesos.Width = 50;
+            dataGridView2.Columns.Add(pesos);
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            limpiar();
+            int no_entradas = int.Parse(textBox1.Text);
+            string numero;
+            
+
+            for (int i = 1; i <= no_entradas; i++)
+            {
+                DataGridViewTextBoxColumn entradas = new DataGridViewTextBoxColumn();
+                entradas.HeaderText="X" + i.ToString();
+                entradas.Width = 50;
+                dataGridView1.Columns.Add(entradas);
+
+
+
+            }
+
+            DataGridViewTextBoxColumn salida = new DataGridViewTextBoxColumn();
+            salida.HeaderText = "Y";
+            salida.Width = 50;
+            dataGridView1.Columns.Add(salida);
+
+            rows = Convert.ToInt32(Math.Pow(2, no_entradas));
+            
+
+           for(int i=0;i<rows;i++)
+            {
+                numero = "";
+                numero=deci_bin(i);
+                
+
+
+                while (numero.Length!=no_entradas)
+                {
+                    numero = "0" + numero;
+
+                }
+
+
+                
+                dataGridView1.Rows.Add();
+                for (int j = 0; j < numero.Length; j++)
+                {
+                    this.dataGridView1.Rows[i].Cells[j].Value = numero[j];
+
+                }
+
+
+
+
+
+
+
+            }
+
+            
+            
+
+
+
+
+
+        }
+
+        public string deci_bin(int deci)
+        {
+            
+            String cadena = "";
+            
+            while (deci > 0)
+                {
+                    if (deci % 2 == 0)
+                    {
+                        cadena = "0" + cadena;
+                    }
+                    else
+                    {
+                        cadena = "1" + cadena;
+                    }
+                    deci = (int)(deci / 2);
+                }
+
+                
+            
+                return cadena;
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int x=0;
+            int w=0;
+            int umbral = int.Parse(textBox2.Text);
+            int acumulador = 0;
+            no_entradas = int.Parse(textBox1.Text);
+            
+            for (int i = 0; i < rows; i++)
+            {
+                acumulador = 0;
+                
+
+
+
+                
+                for (int j = 0; j < no_entradas; j++)
+                {
+                    x = int.Parse(dataGridView1.Rows[i].Cells[j].Value.ToString());
+                    w = Convert.ToInt32(dataGridView2.Rows[j].Cells[0].Value);
+
+                    acumulador += x * w;
+
+                    Console.WriteLine(x);
+                    Console.WriteLine(w);
+                   
+
+
+
+                }
+                Console.WriteLine(acumulador+"\n");
+
+
+                if (acumulador>umbral)
+                {
+
+                    dataGridView1.Rows[i].Cells[no_entradas].Value = 1;
+                }
+                else
+                {
+                    dataGridView1.Rows[i].Cells[no_entradas].Value = 0;
+                }
+
+
+
+
+
+
+
+            }
+
+        }
+
+        public void limpiar()
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                dataGridView1.Rows.Clear();
+                dataGridView1.Columns.Clear();
+                dataGridView2.Rows.Clear();
+                textBox2.Text = "";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            limpiar();
+
+        }
+    }
+}
